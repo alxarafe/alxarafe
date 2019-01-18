89d828f1f96a45ba33eb62804ddba50a44c574c4
Alxarafe <info@alxarafe.com>
2019-01-18 14:11:41 +0100

    PHP-CS fix
    Signed-off-by: Alxarafe <info@alxarafe.com>

--- src/Core/Database/SqlHelpers/SqlMySql.php
+++ src/Core/Database/SqlHelpers/SqlMySql.php
@@ -75,7 +75,8 @@ class SqlMySql extends SqlHelper
      *
      * @return string
      */
-    public function toNativeForm(/** @scrutinizer ignore-unused */array $row = []): string {
+    public function toNativeForm(/** @scrutinizer ignore-unused */array $row = []): string
+    {
         return '';
         /*
           if ($type == 'string') {
