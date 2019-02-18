9273c0a3a6d35f6ca202126ce3cb3695e4833c62
Alxarafe <info@alxarafe.com>
2019-02-18 12:11:33 +0100

    Minor changes in entry by index.php

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
 /**
  * Alxarafe. Development of PHP applications in a flash!
- * Copyright (C) 2018 Alxarafe <info@alxarafe.com>
+ * Copyright (C) 2018-2019 Alxarafe <info@alxarafe.com>
  */
-
 define('BASE_PATH', __DIR__);
+define('DEBUG', true);
 
 $autoload = constant('BASE_PATH') . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 if (file_exists($autoload)) {
     require_once $autoload;
 
-    $bootStrap = new \Alxarafe\BootStrap(constant('BASE_PATH'), true);
+    $bootStrap = new \Alxarafe\BootStrap(constant('BASE_PATH'), constant('DEBUG'));
     $bootStrap->init();
 } else {
-    echo "You need to execute 'composer install' before to start";
+    echo "You need install composer and execute 'composer install' before to start";
 }
