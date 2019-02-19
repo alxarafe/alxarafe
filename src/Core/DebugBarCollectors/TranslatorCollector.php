42a05081ec7fc86342cb9ad6868ecbfe5723fe98
Alxarafe <info@alxarafe.com>
2019-02-19 18:01:48 +0100

    Moved to new resources folder
    Signed-off-by: Alxarafe <info@alxarafe.com>

--- src/Core/DebugBarCollectors/TranslatorCollector.php
+++ src/Core/DebugBarCollectors/TranslatorCollector.php
@@ -84,8 +84,8 @@ class TranslatorCollector extends DataCollector implements Renderable, AssetProv
     public function getAssets()
     {
         return [
-            'css' => baseUrl('/templates/js/phpdebugbar.custom-widget.css'),
-            'js' => baseUrl('/templates/js/phpdebugbar.custom-widget.js'),
+            'css' => baseUrl('/resources/templates/js/phpdebugbar.custom-widget.css'),
+            'js' => baseUrl('/resources/templates/js/phpdebugbar.custom-widget.js'),
         ];
     }
 
