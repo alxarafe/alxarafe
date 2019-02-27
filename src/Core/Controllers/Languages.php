87d34ba51c53fbf304d958c10902782b9d1e07d0
Alxarafe <info@alxarafe.com>
2019-02-27 13:07:43 +0100

    The preprocessor for languages is created, although it is quite possible that it is not the final one. Its usefulness, at the moment is to obtain a list of all the chains, translated or not, in each of the available languages.

--- src/Core/Controllers/Languages.php
+++ src/Core/Controllers/Languages.php
@@ -8,8 +8,8 @@ namespace Alxarafe\Controllers;
 
 use Alxarafe\Base\AuthPageExtendedController;
 use Alxarafe\Models\Language;
+use Alxarafe\PreProcessors\Languages as PreLanguages;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Yaml\Yaml;
 
 /**
  * Class Languages
@@ -36,20 +36,25 @@ class Languages extends AuthPageExtendedController
     {
         $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
         switch ($action) {
-            case 'regenerate':
-                $this->regenerateAction();
+            case 'export':
+                $this->exportAction();
                 break;
         }
         return parent::indexMethod();
+    }
+
+    private function exportAction()
+    {
+        PreLanguages::exportLanguages();
     }
 
     public function getNewButtons()
     {
         $return = [];
         $return[] = [
-            'link' => $this->url . '&action=regenerate',
+            'link' => $this->url . '&action=export',
             'icon' => 'glyphicon-refresh',
-            'text' => 'regenerate-data',
+            'text' => 'export-data',
             'type' => 'info',
         ];
         return $return;
@@ -71,83 +76,4 @@ class Languages extends AuthPageExtendedController
         return $details;
     }
 
-    /**
-     * TODO: This must be moved to ProProcessors namespace
-     */
-    public function regenerateAction()
-    {
-        $subfolder = '/Languages';
-        $sourceFolders = [
-            constant('ALXARAFE_FOLDER') . $subfolder,
-            constant('BASE_PATH') . $subfolder,
-        ];
-
-        $destinationFolder = constant('BASE_PATH') . '/config/languages';
-        if (!is_dir($destinationFolder)) {
-            \mkdir($destinationFolder, 0777, true);
-        }
-
-        $languages = $this->model->getAllRecords();
-        if (count($languages) == 0) {
-            return;
-        }
-
-        /*
-          $default_lang = $languages[0]['language'];
-          $default_variant = $languages[0]['variant'];
-          if (!isset($default_variant)) {
-          $default_variant = strtoupper($default_lang);
-          }
-
-          $defaultFileName = $default_lang . '_' . $default_variant;
-         */
-        $defaultFileName = $languages[0]['language'];
-        foreach ($languages as $lang) {
-            if (!isset($lang['variant'])) {
-                $lang['variant'] = strtoupper($lang['language']);
-            }
-
-            $fileName = $lang['language'] . '_' . $lang['variant'];
-            $this->debugTool->addMessage('messages', 'Processing ' . $fileName . ' language');
-            // echo '<p>Processing ' . $lang['language'] . '_' . $lang['variant'] . ' language</p>';
-
-            $subLanguage = null;
-            if ($lang['variant'] != strtoupper($lang['language'])) {
-                $subLanguage = $lang['language'] . '_' . strtoupper($lang['language']);
-            }
-
-            $allData = [];
-            foreach ($sourceFolders as $source) {
-                $data = [];
-
-                // Se carga el array más general o por defecto (p.e. en inglés)
-                $default = $source . '/' . $defaultFileName . '.yaml';
-                // echo '<p>Processing ' . $default . '</p>';
-                if (file_exists($default)) {
-                    $data = Yaml::parse(file_get_contents($default));
-                }
-
-                // Si es un idioma dependiente, se intenta cargar el genérico
-                // Así por ejemplo, para es_AR se cargaría el es_ES que será mejor que el en_EN
-                if (isset($subLanguage)) {
-                    $file = $source . '/' . $subLanguage . '.yaml';
-                    // echo '<p>Processing ' . $file . '</p>';
-                    if (file_exists($file)) {
-                        $data = array_merge($data, Yaml::parse(file_get_contents($file)));
-                    }
-                }
-
-                // Ya por último, se carga el idioma que hemos solicitado, p.e. es_AR
-                $file = $source . '/' . $fileName . '.yaml';
-                // echo '<p>Processing ' . $file . '</p>';
-                if (file_exists($file)) {
-                    $data = array_merge($data, Yaml::parse(file_get_contents($file)));
-                }
-
-                $allData = array_merge($allData, $data);
-            }
-            ksort($allData);
-            file_put_contents($destinationFolder . '/' . $fileName . '.yaml', Yaml::dump($allData));
-        }
-    }
 }
