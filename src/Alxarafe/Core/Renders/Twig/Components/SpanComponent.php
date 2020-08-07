42a0cdacb205d9320f131e837a12a5855b9299cb
rsanjose <rsanjoseo@gmail.com>
2020-08-07 12:51:08 +0200

    To do the component tests, an array of components is generated and implemented in the record view

---
+++ src/Alxarafe/Core/Renders/Twig/Components/SpanComponent.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Alxarafe. Development of PHP applications in a flash!
+ * Copyright (C) 2018-2020 Alxarafe <info@alxarafe.com>
+ */
+
+namespace Alxarafe\Core\Renders\Twig\Components;
+
+/**
+ * Class Button component
+ *
+ * @package Alxarafe\Core\Renders\Twig\Components
+ */
+class SpanComponent extends AbstractComponent
+{
+    /**
+     * Contains the text for this component.
+     *
+     * @var string
+     */
+    public $text;
+
+    /**
+     * Contains the type for this component.
+     *
+     * @var string
+     */
+    public $type;
+
+    /**
+     * Contains the link for this component.
+     *
+     * @var string
+     */
+    public $link;
+
+    /**
+     * Return the template path to render this component.
+     *
+     * @return string
+     */
+    public function getTemplatePath(): string
+    {
+        return '@Core/components/button.html';
+    }
+}
