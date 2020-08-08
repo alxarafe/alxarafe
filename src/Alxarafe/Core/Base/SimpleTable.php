166b56b7f50bc1e9a6d1c9e3dc6aa6b3a54bf144
rsanjose <rsanjoseo@gmail.com>
2020-08-08 18:50:39 +0200

    setStructure should only be invoked when the yaml files have been removed from the config/structure folder

--- src/Alxarafe/Core/Base/SimpleTable.php
+++ src/Alxarafe/Core/Base/SimpleTable.php
@@ -29,7 +29,8 @@ class SimpleTable extends FlatTable
     {
         parent::__construct($tableName, $params);
         $this->debugTool->startTimer($this->modelName, $this->modelName . ' SimpleTable Constructor');
-        $this->setStructure();
+        // TODO: setStructure should only be invoked when the yaml files have been removed from the config/structure folder
+        // $this->setStructure();
         if (!isset($this->idField) && Database::getInstance()->getDbEngine()->issetDbTableStructureKey($this->tableName, 'fields')) {
             $this->idField = 'id';
             foreach (Database::getInstance()->getDbEngine()->getDbTableStructure($this->tableName)['fields'] as $key => $value) {
