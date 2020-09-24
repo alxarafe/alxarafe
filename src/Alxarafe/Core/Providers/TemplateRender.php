85f18af0d2d28710cb6db26c745c26b2ed4deaf0
Alxarafe <info@alxarafe.com>
2020-09-24 15:24:30 +0200

    - Trying to reduce complexity with tiny methods

--- src/Alxarafe/Core/Providers/TemplateRender.php
+++ src/Alxarafe/Core/Providers/TemplateRender.php
@@ -211,13 +211,25 @@ class TemplateRender
         $result = [];
         foreach ($folders as $key => $folder) {
             $fullFolder = $folder['path'] . DIRECTORY_SEPARATOR . self::TEMPLATES_FOLDER;
-//            FileSystemUtils::mkdir($result[$folder['name']], 0777, true);
-            if (file_exists($fullFolder) && is_dir($fullFolder)) {
-                $result[$folder['name']] = $fullFolder;
-                DebugTool::getInstance()->addMessage('messages', 'Added template render folder ' . $result[$folder['name']]);
-            }
+            $this->addFolder($result, $folder['name'], $fullFolder);
         }
         self::$templatesFolders = array_merge(self::$templatesFolders, $result);
+    }
+
+    /**
+     * Adds a new folder to the list.
+     *
+     * @param array  $list        List of folders
+     * @param string $folderIndex Key of new item
+     * @param string $fullFolder  Value of new item
+     */
+    private function addFolder(array &$list, string $folderIndex, string $fullFolder)
+    {
+        // FileSystemUtils::mkdir($result[$folder['name']], 0777, true);
+        if (file_exists($fullFolder) && is_dir($fullFolder)) {
+            $list[$folderIndex] = $fullFolder;
+            DebugTool::getInstance()->addMessage('messages', 'Added template render folder ' . $list[$folderIndex]);
+        }
     }
 
     /**
@@ -322,16 +334,29 @@ class TemplateRender
             self::$loader->addPath(basePath(self::TEMPLATES_FOLDER), 'Core');
 
             foreach (self::$templatesFolders as $moduleName => $modulePath) {
-//                FileSystemUtils::mkdir($modulePath, 0777, true);
-                if (file_exists($modulePath) && is_dir($modulePath)) {
-                    // Adds without namespace
-                    self::$loader->prependPath($modulePath);
-                    // Adds with namespace Module + $modulePath
-                    self::$loader->prependPath($modulePath, 'Module' . $moduleName);
-                }
+                self::addPath($modulePath, $moduleName);
             }
         } catch (LoaderError $e) {
             Logger::getInstance()::exceptionHandler($e);
+        }
+    }
+
+    /**
+     * Add the module folder to render paths.
+     *
+     * @param string $modulePath
+     * @param string $moduleName
+     *
+     * @throws LoaderError
+     */
+    private function addPath(string $modulePath, string $moduleName)
+    {
+        // FileSystemUtils::mkdir($modulePath, 0777, true);
+        if (file_exists($modulePath) && is_dir($modulePath)) {
+            // Adds without namespace
+            self::$loader->prependPath($modulePath);
+            // Adds with namespace Module + $modulePath
+            self::$loader->prependPath($modulePath, 'Module' . $moduleName);
         }
     }
 
