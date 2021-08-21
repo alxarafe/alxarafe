aa55bc0e80671292d7d652705fd7dcb281835181
rsanjoseo <info@rsanjoseo.com>
2021-08-21 12:01:50 +0200

    Scrutinizer, test 12:01

--- htdocs/Core/Base/Provider.php
+++ htdocs/Core/Base/Provider.php
@@ -16,8 +16,19 @@ abstract class Provider extends Singleton
      * @var string
      */
     private static string $basePath;
+
+    /**
+     * Contains an associative array with the default values of each descendant, indexed by the name of the class.
+     *
+     * @var array
+     */
     private static array $defaultValues;
 
+    /**
+     * Provider constructor
+     *
+     * @param string $index
+     */
     public function __construct(string $index = 'main')
     {
         parent::__construct($index);
@@ -29,8 +40,27 @@ abstract class Provider extends Singleton
         }
     }
 
+    /**
+     * Returns an associative array with the default values for the class.
+     *
+     * @return array
+     */
     abstract function getDefaultValues(): array;
 
+    /**
+     * The object is created from within the class itself only if the class
+     * has no instance.
+     *
+     * We opted to use an array to make several singletons according to the
+     * index passed to getInstance
+     *
+     * Unlike the Singleton class, Provider can read and write data in the
+     * configuration file.
+     *
+     * @param string $index
+     *
+     * @return mixed
+     */
     public static function getInstance(string $index = 'main')
     {
         self::$basePath = constant('BASE_FOLDER') . '/config';
