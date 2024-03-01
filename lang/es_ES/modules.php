c99b64dedb0d677dceb5f84c3e24d92c9043a144
Cayetano H. Osma <cayetano.hernandez@velocitech.io>
2024-03-01 23:10:51 +0000

    Config-stage - Remove useless comment

--- lang/es_ES/modules.php
+++ lang/es_ES/modules.php
@@ -1,16 +1,6 @@
 <?php
 
-declare(strict_types=1);
 
-/*
- * This file is part of PHP CS Fixer.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
 
 return [
     'core-module-not-enabled' => 'Core Module must be enabled to run Alxarafe properly',
