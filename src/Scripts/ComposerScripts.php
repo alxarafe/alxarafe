cd154d64c0e810695407b6e703077e37f4b3320f
Rafael San José Tovar <rsanjose@alxarafe.com>
2024-07-03 11:19:38 +0200

    Test scripts

--- src/Scripts/ComposerScripts.php
+++ src/Scripts/ComposerScripts.php
@@ -1,10 +1,39 @@
 <?php
 
-namespace AlxRoot;
+/* Copyright (C) 2024       Rafael San José         <rsanjose@alxarafe.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 
-abstract class Installer
+namespace Rsanjoseo\Alxarafe\Scripts;
+
+use Composer\Script\Event;
+
+abstract class ComposerScripts
 {
-    public static function copyAssets()
+    public static function postUpdate(Event $event)
+    {
+        $composer = $event->getComposer();
+        $io = $event->getIO();
+
+        // Perform operations here
+        $io->write("Running post-update script");
+
+        static::copyAssets();
+    }
+
+    private static function copyAssets()
     {
         echo "Starting copyAssets...\n";
 
