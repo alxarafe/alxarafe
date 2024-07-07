4c9f29da424cd8dba2a77932315f2fecc5f0108d
Rafael San José Tovar <rsanjose@alxarafe.com>
2024-07-07 22:13:34 +0200

    Templates that had not been attached are added

---
+++ Templates/common/page/layout/empty.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="{!! $me->lang !!}">
+<head>
+    @include('page.partial.head')
+</head>
+<body class="container">
+@include('page.partial.body_empty')
+@include('page.partial.footer')
+</body>
+</html>
---
+++ Templates/common/page/layout/main.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="{!! $me->lang !!}">
+<head>
+    @include('page.partial.head')
+</head>
+<body class="container">
+@include('page.partial.body_standard')
+@include('page.partial.footer')
+</body>
+</html>
