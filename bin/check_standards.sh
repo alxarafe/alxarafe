8a5c65b673e24c37ac21e283c4b5143668a887ac
Rafael San José <rsanjose@alxarafe.com>
2026-02-07 09:38:22 +0100

    chore: enable PHPMD in check_standards.sh script

--- bin/check_standards.sh
+++ bin/check_standards.sh
@@ -7,6 +7,6 @@ docker exec alxarafe_php ./vendor/bin/phpcs --standard=phpcs.xml Tests
 # docker exec alxarafe_php ./vendor/bin/phpcs --standard=phpcs.xml skeleton/Tests
 
 echo "Running Mess Detector (PHPMD)..."
-# docker exec alxarafe_php ./vendor/bin/phpmd src/Core text cleancode,codesize,controversial,design,naming,unusedcode
-# docker exec alxarafe_php ./vendor/bin/phpmd Tests text cleancode,codesize,controversial,design,naming,unusedcode
+docker exec alxarafe_php ./vendor/bin/phpmd src/Core text cleancode,codesize,controversial,design,naming,unusedcode
+docker exec alxarafe_php ./vendor/bin/phpmd Tests text cleancode,codesize,controversial,design,naming,unusedcode
 # docker exec alxarafe_php ./vendor/bin/phpmd skeleton/Tests text cleancode,codesize,controversial,design,naming,unusedcode
