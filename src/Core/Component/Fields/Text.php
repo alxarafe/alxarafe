42892ccb33d3482662b903029f6eb9ddb2a4b08a
Rafael San José <rsanjose@alxarafe.com>
2026-02-20 22:47:10 +0100

    Fix DebugBar assets and modernize TranslationsWidget
    - Rewrote translations.js in Vanilla JS/ES6 to remove jQuery dependency.
    - Updated Debug.php to use non-minified DebugBar assets and corrected basePath.
    - Added jQuery and footer rendering to public layout.
    - Synchronized assets for Docker environment.

--- src/Core/Component/Fields/Text.php
+++ src/Core/Component/Fields/Text.php
@@ -17,22 +17,6 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-/* Copyright (C) 2026      Rafael San José      <rsanjose@alxarafe.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- */
-
 namespace Alxarafe\Component\Fields;
 
 use Alxarafe\Component\AbstractField;
